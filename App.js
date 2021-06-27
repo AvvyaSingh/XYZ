4f765de2bbf97566234230e9fc90c379e5f78a38
Avvya Pratap Singh <avvyaking1234@gmail.com>
2021-06-27 15:22:33 +0530

    message

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
 import SignupLoginScreen from './screens/SignupLoginScreen';
 
 export default function App() {
@@ -7,4 +8,13 @@ export default function App() {
     <SignupLoginScreen/>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
